3e1819c7a382dadce55fa22da1af97116eb8af44
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-02-27 00:32:21 +0100

    Fix docs/conf.py messed up by bump2version -.-"

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ year = '2019'
 copyright = '{0}, {1}'.format(year, author)
 
 # The short X.Y version
-version = 'version = release = '0.1.1''
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
